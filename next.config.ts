e18a49387b17c2746b2ef34edb54a4c22501dbb8
firli <firli.khoirunnisa777@gmail.com>
2025-08-01 12:16:41 +0700

    new8

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // ← penting untuk static export
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
